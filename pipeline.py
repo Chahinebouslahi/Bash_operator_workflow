b4fcc82a6752d2859c423efa3b6e20243d3c7ea4
chahinebouslahi <bouslahichahine02@gmail.com>
2025-12-19 22:29:31 +0100

    setting arguments

--- pipeline.py
+++ pipeline.py
@@ -1,3 +1,14 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
+
+# Default DAG arguments
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 1, 1),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
