62cb809ce9028ac001a2a36d29f0b37383d6f260
chahinebouslahi <bouslahichahine02@gmail.com>
2025-12-19 22:37:16 +0100

    deleting email param

--- pipeline.py
+++ pipeline.py
@@ -6,9 +6,6 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2024, 1, 1),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=1),
 }
